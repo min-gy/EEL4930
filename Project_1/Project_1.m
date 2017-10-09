1e533a0328d3b5f02544a4ab8325aea1e1684c0c
Ming Yang <mingyang2.71@gmail.com>
2017-10-09 11:37:03 -0400

    about to stash it

--- Project_1/Project_1.m
+++ Project_1/Project_1.m
@@ -10,7 +10,7 @@ function project_1()
     a = 0.001;
     filler = randn(1,M);
     w = zeros(size(X,1), M);
-    %w(1,:) = filler;
+    w(1,:) = filler;
     Y = zeros(size(X, 1), 1);
     J = zeros(size(X, 1), 1);
     e = zeros(size(X, 1), 1);
@@ -36,21 +36,21 @@ function project_1()
         Y(i) = w(i,:)*X_col_vec;
         e(i) = d(i)-Y(i);
         J(i) = e(i)^2;
-        temp = X(i);
-        temp_e = e(i);
-        temp_w = w(i,:);
-        temp_nextw = w(i+1,:);
-        %w(i+1,:) = w(i,:) + 2*a*e(i)*temp;
-        temp_nextw = temp_w + 2*a*temp_e*temp;
+        %temp = X(i);
+        %temp_e = e(i);
+        %temp_w = w(i,:);
+        %temp_nextw = w(i+1,:);
+        w(i+1,:) = w(i,:) + 2*a*e(i)*X(i);
+        %temp_nextw = temp_w + 2*a*temp_e*temp;
     end
     
-    sound(e,fs);
+    %sound(e,fs);
     
     hold;
     for i = 1:M
         plot(w(:,i));
     end
-    plot(J);
+    %plot(J);
         
 
 
