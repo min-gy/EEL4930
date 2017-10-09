fa96a4ed0d8507612e4c8faa2c531842a6dc7c19
Ming Yang <mingyang2.71@gmail.com>
2017-10-09 11:35:46 -0400

    improved filter, started report

--- Project_1/Project_1.m
+++ Project_1/Project_1.m
@@ -6,52 +6,84 @@ function project_1()
     
     %define filter order M, step size a, and initialize weights with
     %randn()
-    M = 5;
-    a = 0.001;
-    filler = randn(1,M);
-    w = zeros(size(X,1), M);
-    %w(1,:) = filler;
-    Y = zeros(size(X, 1), 1);
+    a = 0.0001;
+    stepsize = 5;
+    max_order = 100;
+    M= 150;
+    
+    erle_plot_index = [5:stepsize:max_order];
+    erle = zeros(max_order/stepsize,1);
+    erle_counter = 1;
+    w = zeros(size(X,1), max_order);
     J = zeros(size(X, 1), 1);
-    e = zeros(size(X, 1), 1);
     
-    X_col_vec = zeros(M,1);
-    
-    %step through sample i in the time series
-    for i = 1:size(X,1)
-        if i < M+1
-            X_col_vec = zeros(M, 1);
-            j = 0;
-            if i>1
-                for j = 1:i-1
-                    X_col_vec(j) = X(i-j);
-                end
-            end
-        else
-            for j = 1:M
-                X_col_vec(j) = X(i-j);
-            end
-        end
-        %X_col_vec = repmat(X(i), M, 1); 
-        Y(i) = w(i,:)*X_col_vec;
-        e(i) = d(i)-Y(i);
-        J(i) = e(i)^2;
-        temp = X(i);
-        temp_e = e(i);
-        temp_w = w(i,:);
-        temp_nextw = w(i+1,:);
-        %w(i+1,:) = w(i,:) + 2*a*e(i)*temp;
-        temp_nextw = temp_w + 2*a*temp_e*temp;
-    end
-    
-    sound(e,fs);
-    
-    hold;
-    for i = 1:M
-        plot(w(:,i));
-    end
-    plot(J);
-        
+%     for M = 5:stepsize:max_order
+        %filler = randn(1,M);
+        w = zeros(size(X,1), M);
+        %w(1,:) = filler;
+        Y = zeros(size(X, 1), 1);
+        J = zeros(size(X, 1), 1);
+        e = zeros(size(X, 1), 1);
+        X_col_vec = zeros(M,1);
 
+        for times = 1:4   
+            w(1,:) = w(size(X,1),:);
+            %step through sample i in the time series
+            for i = 1:size(X,1)
+                if i < M+1
+                    X_col_vec = zeros(M, 1);
+                    j = 0;
+                    if i>1
+                        for j = 1:i-1
+                            X_col_vec(j) = X(i-j);
+                        end
+                    end
+                else
+                    for j = 1:M
+                        X_col_vec(j) = X(i-j);
+                    end
+                end
+                Y(i) = w(i,:)*X_col_vec;
+                e(i) = d(i)-Y(i);
+                J(i) = e(i)^2;
+                w(i+1,:) = w(i,:) + (2*a*e(i)*X_col_vec).';
+            end
+            erle(erle_counter) = 10*log10((d.'*d)/(e.'*e));
+            erle_counter = erle_counter+1;
+            subplot(2,2,times);
+            
+            hold on;
+            for b = 1:M
+                plot(w(:,b));
+            end
+            hold off;
+        end
+%     end
+    
+%     subplot(2,2,3);
+%     plot(erle_plot_index, erle);
+% 
+%     %plots
+%     subplot(2,2,1);
+%     hold;
+%     for i = 1:M
+%         plot(w(:,i));
+%     end
+% 
+%     subplot(2,2,2);
+%     plot(J);
+      sound(e);
+
+%     subplot(2,2,1)
+%     plot(d, 'green');
+%     hold;
+%     plot(e, 'red');
+%     legend('music+voice','error');
+%    
+%     subplot(2,2,3);
+%     plot(d, 'green');
+%     hold;
+%     plot(X, 'black');
+%     legend('music+voice','music');
 
 end
